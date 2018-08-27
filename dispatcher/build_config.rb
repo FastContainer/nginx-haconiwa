0439bc32931c1a9ba0e41b41da132fbf89732e85
linyows <linyows@gmail.com>
2018-08-27 18:52:32 +0900

    :lipstick:

--- dispatcher/build_config.rb
+++ dispatcher/build_config.rb
@@ -1,33 +1,14 @@
 def gem_config(conf)
-  #conf.gembox 'default'
-
-  # be sure to include this gem (the cli app)
+  conf.gem github: 'iij/mruby-socket'
+  conf.gem github: 'matsumotory/mruby-mutex'
+  conf.gem github: 'matsumotory/mruby-fast-remote-check'
+  conf.gem github: 'mattn/mruby-http'
+  conf.gem github: 'katzer/mruby-shelf'
+  conf.gem github: 'haconiwa/haconiwa', checksum_hash: ENV['HACONIWA_VERSION'], branch: 'master'
   conf.gem File.expand_path(File.dirname(__FILE__))
 end
 
 MRuby::Build.new do |conf|
   toolchain :gcc
-
-  conf.gem mgem: 'mruby-io'
-  conf.gem mgem: 'mruby-env'
-  conf.gem mgem: 'mruby-dir'
-  conf.gem mgem: 'mruby-digest'
-  conf.gem mgem: 'mruby-process'
-  conf.gem mgem: 'mruby-pack'
-  conf.gem mgem: 'mruby-socket'
-  conf.gem mgem: 'mruby-json'
-  conf.gem mgem: 'mruby-onig-regexp'
-  conf.gem mgem: 'mruby-redis'
-  conf.gem mgem: 'mruby-vedis'
-  conf.gem mgem: 'mruby-sleep'
-  conf.gem mgem: 'mruby-userdata'
-  conf.gem mgem: 'mruby-uname'
-  conf.gem mgem: 'mruby-httprequest'
-  conf.gem mgem: 'mruby-mutex'
-  conf.gem mgem: 'mruby-localmemcache'
-  conf.gem mgem: 'mruby-base64'
-  conf.gem mgem: 'mruby-fast-remote-check'
-  conf.gem github: 'haconiwa/haconiwa'
-
   gem_config(conf)
 end
