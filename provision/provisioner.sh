e3f55d51bc2d2727c76ac87bc57b25c9132da02c
linyows <linyows@gmail.com>
2018-09-03 18:14:53 +0900

    create rootfs dir

--- provision/provisioner.sh
+++ provision/provisioner.sh
@@ -19,10 +19,9 @@ rm -rf /var/lib/haconiwa/hacos && ln -s /data/hacos /var/lib/haconiwa/hacos
 
 # deploy container images
 test -d /var/lib/haconiwa/rootfs || mkdir -p /var/lib/haconiwa/rootfs
-test -d /var/log/haconiwa || mkdir -p /var/log/haconiwa
 for ((i = 0; i < ${#images[@]}; i++)) {
-  test -d /var/lib/haconiwa/rootfs/${images[i]} || mkdir /var/lib/haconiwa/rootfs/${images[i]}
-  tar xfp /data/dist/${images[i]}.image.tar -C /var/lib/haconiwa/rootfs/${images[i]}
+  test -d /var/lib/haconiwa/images/${images[i]} || mkdir -p /var/lib/haconiwa/images/${images[i]}
+  tar xfp /data/dist/${images[i]}.image.tar -C /var/lib/haconiwa/images/${images[i]}
 }
 
 # setup network
