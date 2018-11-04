572105bf5ef14f348c943deff4606eaced61668a
linyows <linyows@gmail.com>
2018-11-04 22:41:12 +0900

    use native for MX

--- container/postfix/entry.sh
+++ container/postfix/entry.sh
@@ -5,6 +5,8 @@ postconf -F '*/*/chroot = n'
 
 # allow private network
 postconf -e mynetworks='127.0.0.0/8 192.168.0.0/16 172.16.0.0/12 10.0.0.0/8'
+# lookup hosts
+postconf -e smtp_host_lookup='dns native'
 
 if [ "$bench" == "true" ]; then
   # for smtp-sink
--- provision/smtp.sh
+++ provision/smtp.sh
@@ -8,5 +8,11 @@ apt update -y
 apt install -y postfix curl
 locale-gen ja_JP.UTF-8
 
+postconf -e myhostname="$(hostname)"
+postconf -e mynetworks='127.0.0.0/8 192.168.0.0/16 172.16.0.0/12 10.0.0.0/8'
+postconf -e smtp_host_lookup='dns native'
+
+systemctl restart postfix
+
 # netdata
 bash <(curl -Ss https://my-netdata.io/kickstart.sh) all --non-interactive
