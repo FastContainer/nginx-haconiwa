ee191577db35d66a45aa3703faf73e7f1990eb2b
linyows <linyows@gmail.com>
2018-11-16 18:50:39 +0900

    add provisioning script for tarpit

---
+++ provision/smtp-tarpit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-get update -yy
+apt-get install -yy make build-essential libev-dev
+locale-gen ja_JP.UTF-8
+
+test -d /usr/src/mxtarpit || \
+  git clone https://github.com/martinh/mxtarpit.git /usr/src/mxtarpit
+cd /usr/src/mxtarpit
+make linux
+mv ./mxtarpit /usr/sbin/mxtarpit
+test -f /etc/systemd/system/mxtarpit.service || \
+  cp /tmp/mxtarpit.service /etc/systemd/system/mxtarpit.service && systemctl daemon-reload
+systemctl start mxtarpit
