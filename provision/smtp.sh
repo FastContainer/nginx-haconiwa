d62bcc7f218fc180ea566e01fa635359822b4b4e
linyows <linyows@gmail.com>
2018-11-21 19:16:33 +0900

    default_process_limit is 5

--- provision/smtp.sh
+++ provision/smtp.sh
@@ -12,5 +12,6 @@ postconf -e myhostname="$(hostname)"
 postconf -e mynetworks='127.0.0.0/8 192.168.0.0/16 172.16.0.0/12 10.0.0.0/8'
 postconf -e smtp_host_lookup='native'
 postconf -e smtp_dns_support_level='disabled'
+postconf -e default_process_limit='5'
 
 systemctl restart postfix
