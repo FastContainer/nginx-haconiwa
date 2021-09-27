c54c320ce8682353c23206c14a8f37bf9272123f
linyows <linyows@gmail.com>
2021-09-27 18:21:57 +0900

    move code to batch.sh

--- provision/containers/batch.sh
+++ provision/containers/batch.sh
@@ -1,5 +1,51 @@
-#!/bin/bash
+#!/bin/bash -x
 
+sysctl -w net.core.somaxconn=4096
+ulimit -n 100000
+
+# expand postfix image
+postfix_rootfs_path=/var/lib/haconiwa/rootfs/shared/postfix
+postfix_image_path=/var/lib/haconiwa/images/postfix.image.tar
+postfix_workdir_path=/var/lib/haconiwa/images/postfix-workdir.tar.gz
+test -d ${postfix_rootfs_path} || mkdir -m 755 -p ${postfix_rootfs_path}
+test -n "`ls ${postfix_rootfs_path}`" || tar xfp ${postfix_image_path} -C ${postfix_rootfs_path}
+for i in `seq 2 201`; do
+  path=/var/lib/haconiwa/rootfs/postfix-10-1-1-${i}
+  if [ ! -d $path ]; then
+    mkdir -m 755 -p ${path} && tar xzfp ${postfix_workdir_path} -C ${path}
+  fi
+  sleep 1
+done
+for i in `seq 2 201`; do
+  path=/var/lib/haconiwa/rootfs/postfix-10-1-2-${i}
+  if [ ! -d $path ]; then
+    mkdir -m 755 -p ${path} && tar xzfp ${postfix_workdir_path} -C ${path}
+  fi
+  sleep 1
+done
+for i in `seq 2 201`; do
+  path=/var/lib/haconiwa/rootfs/postfix-10-1-3-${i}
+  if [ ! -d $path ]; then
+    mkdir -m 755 -p ${path} && tar xzfp ${postfix_workdir_path} -C ${path}
+  fi
+  sleep 1
+done
+for i in `seq 2 201`; do
+  path=/var/lib/haconiwa/rootfs/postfix-10-1-4-${i}
+  if [ ! -d $path ]; then
+    mkdir -m 755 -p ${path} && tar xzfp ${postfix_workdir_path} -C ${path}
+  fi
+  sleep 1
+done
+for i in `seq 2 201`; do
+  path=/var/lib/haconiwa/rootfs/postfix-10-1-5-${i}
+  if [ ! -d $path ]; then
+    mkdir -m 755 -p ${path} && tar xzfp ${postfix_workdir_path} -C ${path}
+  fi
+  sleep 1
+done
+
+# start postfix
 for i in `seq 2 201`; do
   env IP=10.1.1.${i} \
       ID=postfix-10-1-1-${i} \
